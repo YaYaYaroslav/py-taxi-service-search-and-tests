787b99f0be7b576b916c09f2726ec8003ea47f01
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2024-11-28 19:51:17 +0200

    add tests

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from taxi.models import Driver, Car, Manufacturer
 
-# Create your tests here.
+class SearchTests(TestCase):
+    def setUp(self):
+        self.driver = Driver.objects.create_user(username="test_driver", password="password123")
+        self.car = Car.objects.create(model="Toyota")
+        self.manufacturer = Manufacturer.objects.create(name="Toyota", country="Japan")
+
+    def test_driver_search(self):
+        response = self.client.get(reverse("taxi:driver-list") + "?q=test_driver")
+        self.assertContains(response, "test_driver")
+
+    def test_car_search(self):
+        response = self.client.get(reverse("taxi:car-list") + "?q=Toyota")
+        self.assertContains(response, "Toyota")
+
+    def test_manufacturer_search(self):
+        response = self.client.get(reverse("taxi:manufacturer-list") + "?q=Toyota")
+        self.assertContains(response, "Toyota")
